460db74b5fa969a191888e7e3daca199c9039476
tjs04 <81762786+t01js@users.noreply.github.com>
2023-05-12 19:38:17 +0200

    Create food.py

---
+++ food.py
@@ -0,0 +1,9 @@
+class Food:
+    def __init__(self):
+        self.randomize_position()
+
+    def randomize_position(self):
+        self.position = (random.randint(0, 63) * 10, random.randint(0, 47) * 10)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), (self.position[0], self.position[1], 10, 10))
